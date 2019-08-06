0491856227d746eaa834029a48567eb04aa26386
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2019-08-05 21:21:51 -0400

    added webpack css loader configs, and build ready for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtract = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 
@@ -21,7 +22,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+        use: [
+          MiniCSSExtract.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+
       }
     ]
   },
@@ -29,6 +37,10 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new MiniCSSExtract({
+      filename: 'app.css',
+      chunkFilename: '[id].css',
+    }),
   ]
 };
